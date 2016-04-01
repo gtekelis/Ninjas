b8241cca736b1b2e221e33515ad6993d5653165a
George Tekelis <tekelisg@gmail.com>
2016-04-01 16:43:15 +0300

    ok

--- NinjaDomain.Classes/NinjaEquipment.cs
+++ NinjaDomain.Classes/NinjaEquipment.cs
@@ -24,7 +24,7 @@ namespace NinjaDomain.Classes
             set
             {
                 throw new NotImplementedException();
-            }
+            }   
         }
         public DateTime DateCreated
         {
